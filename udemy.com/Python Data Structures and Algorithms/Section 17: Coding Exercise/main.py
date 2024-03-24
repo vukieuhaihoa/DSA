baeb51c8c4e7da4a0cf4ced583286016a4cc02c4
Vu Kieu Hai Hoa <vukieuhaihoa@gmail.com>
2024-03-24 22:31:38 +0700

    feat: Udemy - Python DSA - section 17 - 24/03/2024

---
+++ udemy.com/Python Data Structures and Algorithms/Section 17: Coding Exercise/main.py
@@ -0,0 +1,53 @@
+class HashTable:
+    def __init__(self, size = 7):
+        self.data_map = [None] * size
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash  
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map): 
+            print(i, ": ", val)
+
+    def set(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+    
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for pair in self.data_map[index]:
+                if pair[0] == key: return pair[1]
+        return None
+    
+    def keys(self):
+        all_keys = []
+        for _, lst in enumerate(self.data_map):
+            if lst:
+                for pair in lst: all_keys.append(pair[0])
+        return all_keys
+        
+            
+my_hash_table = HashTable()
+
+my_hash_table.print_table()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    0 :  None
+    1 :  None
+    2 :  None
+    3 :  None
+    4 :  None
+    5 :  None
+    6 :  None
+
+"""
